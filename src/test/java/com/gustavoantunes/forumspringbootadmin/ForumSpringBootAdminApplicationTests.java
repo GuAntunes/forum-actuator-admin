13423cc98ba472dd239a340719fa13d118854d5a
Gustavo Antunes <gas.antunes84@gmail.com>
2020-01-19 15:33:10 -0300

    Criado aplicação spring boot configurado com spring boot admin

---
+++ src/test/java/com/gustavoantunes/forumspringbootadmin/ForumSpringBootAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavoantunes.forumspringbootadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumSpringBootAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
